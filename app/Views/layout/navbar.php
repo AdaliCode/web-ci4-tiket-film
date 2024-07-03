bfa8b5b3f166b22523ba442d90aa800aa2c5ec0b
AdaliCode <fiezayyan@gmail.com>
2024-07-03 07:21:34 +0700

    memperbaiki tampailan home page dan menggunakan multiple views

---
+++ app/Views/layout/navbar.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container">
+        <a class="navbar-brand" href="<?= base_url('/'); ?>">IntiFilm</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item mx-1">
+                    <a class="nav-link active" aria-current="page" href="<?= base_url('/'); ?>">HOME</a>
+                </li>
+                <li class="nav-item mx-1">
+                    <a class="nav-link" href="<?= base_url('/about'); ?>">ABOUT</a>
+                </li>
+                <li class="nav-item mx-1">
+                    <a class="nav-link" href="<?= base_url('/blog'); ?>">BLOG</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
